0c58f773f6c82e71b4d0512b57997d40062019d7
fallenatlas <tiago.a.p.antunes@gmail.com>
2023-12-15 00:02:13 +0000

    feat: implement callback for nodes

---
+++ mgl/mglCallBack.hpp
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// CallBack Abstraction Class
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef MGL_CALLBACK_HPP
+#define MGL_CALLBACK_HPP
+
+#include <GL/glew.h>
+
+namespace mgl {
+
+	class Callback;
+
+	class CallBack {
+	public:
+		virtual void beforeDraw() = 0;
+		virtual void afterDraw() = 0;
+	};
+
+	class SillouetteCallBack : public CallBack {
+	public:
+		void beforeDraw() override;
+		void afterDraw() override;
+	};
+
+}
+
+#endif // !MGL_CALLBACK_HPP
+
---
+++ mgl/mglCallback.cpp
@@ -0,0 +1,25 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Camera Abstraction Class
+//
+// Copyright (c)2023 by Carlos Martinho
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "mglCallBack.hpp"
+
+namespace mgl {
+
+    void SillouetteCallBack::beforeDraw() {
+        glEnable(GL_CULL_FACE);
+        glCullFace(GL_FRONT);
+        glFrontFace(GL_CCW);
+    }
+
+    void SillouetteCallBack::afterDraw() {
+        glEnable(GL_CULL_FACE);
+        glCullFace(GL_BACK);
+        glFrontFace(GL_CCW);
+    }
+
+}
